45f2cd5bdcf64898177c5c8f6c3733b992fb3c6e
Mathew Qin <MathewQin@163.com>
2019-07-06 19:56:12 +0800

    1

--- simulator.py
+++ simulator.py
@@ -21,7 +21,7 @@ class BaseSimulator:
         #u,v,r,dheel,x,y,yaw,heel
         self.state=np.zeros(8)
         self.sailboat_model=SailBoatModel()
-        self.sail_list=np.linspace(-pi,pi,360)
+        self.sail_list=np.linspace(-pi,pi,2*pi)
         self.twind=np.zeros(2)
 
     #邓乃铭
@@ -44,30 +44,26 @@ class FleetRace(BaseSimulator):
         vx=1
         vy=0
         awv=5
-        awa=180
+        awa=pi/4
         yaw,r,x,y=self.state[6],self.state[2],self.state[4],self.state[5]
 
         def true_wind(awv, awa, vx, vy, yaw, r):
             airmar_x = 0.7
-            airmar_vx = vx + sin(radians(yaw)) * r * airmar_x
-            airmar_vy = vy + cos(radians(yaw)) * r * airmar_x
-            true_wind_x = awv * cos(radians(awa + yaw + 180)) + airmar_vx
-            true_wind_y = awv * sin(radians(awa + yaw + 180)) + airmar_vy
+            airmar_vx = vx + sin(yaw) * r * airmar_x
+            airmar_vy = vy + cos(yaw) * r * airmar_x
+            true_wind_x = awv * cos(awa + yaw + pi) + airmar_vx
+            true_wind_y = awv * sin(awa + yaw + pi) + airmar_vy
             twv = sqrt(pow(true_wind_x, 2) + pow(true_wind_y, 2))
             if true_wind_y < 0:
-                twa = degrees(arctan2(true_wind_y, true_wind_x)) + 180
+                twa = arctan2(true_wind_y, true_wind_x) + pi
             else:
-                twa = degrees(arctan2(true_wind_y, true_wind_x)) - 180
+                twa = arctan2(true_wind_y, true_wind_x) - pi
             return twv, twa
 
         def goal_derection(goal_x, goal_y, x, y, twa):  # 目标点坐标goal_x,goal_y，x,y为当前位置坐标
-            goal_angle = degrees(arctan2((goal_y - y), (goal_x - x)))
-            goal_wind_angle = goal_angle - twa  # 风来向右侧为正
-            if goal_wind_angle < -180:
-                goal_wind_angle = goal_wind_angle + 360
-            if goal_wind_angle > 180:
-                goal_wind_angle = goal_wind_angle - 360
-            # print(goal_angle,goal_wind_angle,twa)
+            goal_angle = arctan2((goal_y - y), (goal_x - x))
+            goal_wind_angle = yawRange(goal_angle - twa)  # 风来向右侧为正
+            print(goal_angle,goal_wind_angle,twa)
             return goal_wind_angle
 
         def vpp_selection(twv):  # vpp每个速度对应矩阵形式，每一行为对应角度的各个参数。所有vpp矩阵按速度大小放入vpp列表中,第一列为角度，第二列为速度
@@ -87,16 +83,9 @@ class FleetRace(BaseSimulator):
             return vpp_goal
 
         def v_selection(goal_wind_angle, vpp_goal):
-            v_goalcompute = []
-            for i in range(np.size(vpp_goal, 0)):
-                vpp_angle_compute = vpp_goal[i, 0] - goal_wind_angle
-                if vpp_angle_compute < -180:
-                    vpp_angle_compute = vpp_angle_compute + 360
-                if vpp_angle_compute > 180:
-                    vpp_angle_compute = vpp_angle_compute - 360
-                v_goalcompute.append(vpp_goal[i, 1] * cos(radians(vpp_goal[i, 0] - goal_wind_angle)))
-            v_goal_positon = v_goalcompute.index(max(v_goalcompute))
-            v_angle = vpp_goal[v_goal_positon, 0]
+            v_goalcompute=vpp_goal[:, 1] * cos( vpp_goal[:, 0] - goal_wind_angle)
+            v_goal_positon = np.argmax(v_goalcompute)
+            v_angle = yawRange(vpp_goal[v_goal_positon, 0]+twa)#对地坐标航向角
             v_goal = vpp_goal[v_goal_positon, 1]
             v_sail_angle = vpp_goal[v_goal_positon, 2]
             v_rudder_angle = vpp_goal[v_goal_positon, 3]
@@ -108,6 +97,7 @@ class FleetRace(BaseSimulator):
         vpp_goal = vpp_selection(twv)
         v_angle, v_goal, v_sail_angle, v_rudder_angle, v_heading = v_selection(goal_wind_angle, vpp_goal)
         #v_angle航向角 #v_goal稳定目标速度 #v_sail_angle稳定帆角 #v_rudder_angle稳定舵角
+        print(np.tan(v_angle))
         return v_angle, v_goal, v_sail_angle, v_rudder_angle, v_heading
     
     #秦操
